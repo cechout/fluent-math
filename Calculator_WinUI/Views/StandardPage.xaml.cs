2f285c0226bfdffd8443d1da2762bffd2193bfa9
Daniel Čech <105595226+cechout@users.noreply.github.com>
2026-09-03 23:55:54 +0200

    Make the dotnet format check blocking (#8)
    * chore: pin the checkout to lf line endings
    * chore: fix whitespace violations reported by dotnet format
    * chore: make the format check blocking

--- Calculator_WinUI/Views/StandardPage.xaml.cs
+++ Calculator_WinUI/Views/StandardPage.xaml.cs
@@ -5,7 +5,7 @@ namespace Calculator_WinUI.Views
 {
     public sealed partial class StandardPage : Page
     {
-        public StandardViewModel ViewModel { get; } 
+        public StandardViewModel ViewModel { get; }
 
         public StandardPage()
         {
